790a5680c1bd79d97989ea382ec50a012f52ab7b
Coby <ratchet3789@users.noreply.github.com>
2024-08-01 11:52:33 +1000

    Update Buffer.h
    Removed needless and inaccurate comments

--- Engine/Core/Source/Buffer.h
+++ Engine/Core/Source/Buffer.h
@@ -11,33 +11,23 @@ class Buffer
 public:
 	Buffer(int size)
 	{
-		// Size of our Data Type
 		elementSize = sizeof(T);
-		// Size of our Buffer in Memory based on the item count
 		memSize = elementSize * size;
-		// Allocate that size memory
 		memory = malloc(memSize);
-		// Fill that memory entirely with 0s
 		memset(memory, 0, memSize);
 	}
 
 	~Buffer()
 	{
-		// Clear the memory
 		free(memory);
-		// Ensure no UAF issues
 		memory = nullptr;
 	}
 
 	void Insert(const T& value, int location)
 	{
-		// Get mem position
 		size_t bufferPosition = elementSize * location;
-		// Get the start addr of our Buffer
 		T* memoryStart = (T*)memory;
-		// Cpy our value to the correct memory location
 		memcpy(memoryStart + bufferPosition, &value, elementSize);
-		// Increment the memory count
 		count++;
 	}
 };
